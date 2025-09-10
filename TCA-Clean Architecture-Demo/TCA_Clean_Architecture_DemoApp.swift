72a5418ba3cedb39b6c1a857644ab6c8c50fdf9f
shinseunguk <krdut1@gmail.com>
2025-09-10 11:29:01 +0900

    Initial Commit

---
+++ TCA-Clean Architecture-Demo/TCA_Clean_Architecture_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Clean_Architecture_DemoApp.swift
+//  TCA-Clean Architecture-Demo
+//
+//  Created by ukseung.dev on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Clean_Architecture_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA-Clean Architecture-DemoUITests/TCA_Clean_Architecture_DemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TCA_Clean_Architecture_DemoUITestsLaunchTests.swift
+//  TCA-Clean Architecture-DemoUITests
+//
+//  Created by ukseung.dev on 9/10/25.
+//
+
+import XCTest
+
+final class TCA_Clean_Architecture_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
